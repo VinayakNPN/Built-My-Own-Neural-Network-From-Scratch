065e823f004da8330b5d312a1cd758ab10ae06f6
VinayakNPN <chouhanvinayak86@gmail.com>
2025-08-17 11:19:48 +0530

    Coded a Neuron

--- neural-net.py
+++ neural-net.py
@@ -12,3 +12,9 @@ class Neuron:
         total = np.dot(self.weights,inputs) + self.bias
         return SigMoid(total)
     
+weights = np.array([0,1]) #!w1 = 0 , w2 = 1
+bias = 4
+n = Neuron(weights,bias)
+
+x = np.array([2,3]) #* x1 = 2 , x2 = 3
+print(n.FeedForward(x)) #?0.9990889488055994
