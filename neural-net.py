3a9bcbafac77b6e38c5f9777eeae6896051565e2
VinayakNPN <chouhanvinayak86@gmail.com>
2025-08-17 11:16:24 +0530

    Neuron Class

--- neural-net.py
+++ neural-net.py
@@ -3,3 +3,12 @@ import numpy as np
 def SigMoid(x):
     #*Activation Function formula: f(x) = 1 / (1 + e^(-x))
     return 1 / (1 + np.exp(-x)) 
+
+class Neuron:
+    def __init__(self,weights,bias):
+        self.weights = weights
+        self.bias = bias
+    def FeedForward(self,inputs):
+        total = np.dot(self.weights,inputs) + self.bias
+        return SigMoid(total)
+    
